3e3f23e2189b09200203895d04425ebc070d5e6b
Fábio <fabio.vasconcelos@fabamaq.com>
2023-03-03 10:37:06 +0000

    Task L1.2

--- src/Vehicle_Student.cpp
+++ src/Vehicle_Student.cpp
@@ -24,8 +24,10 @@ void Vehicle::setCurrentDestination(std::shared_ptr<Intersection> destination)
 
 void Vehicle::simulate()
 {
-    // Task L1.2 : Start a thread with the member function „drive“ and the object „this“ as the launch parameters. 
-    // Also, add the created thread into the _thread vector of the parent class. 
+    // Task L1.2 : Start a thread with the member function „drive“ and the object „this“ as the launch parameters.
+    // Also, add the created thread into the _thread vector of the parent class.
+    auto t = std::thread (drive(), this);
+    _threads.push_back(t);
 }
 
 // virtual function which is executed in a thread
